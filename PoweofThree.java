174442bdf7737be3758ffdbbb3ba823c1c8debf2
Sravanthi Kanchi <shravik593@gmail.com>
2017-12-12 15:53:58 -0500

    Power of 3

---
+++ PoweofThree.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+class PowerofThree{
+  
+ public boolean isPowerOfThree(int n) {
+        
+         return n>0 && ((1162261467 % n) ==0); 
+    }
+
+
+}
+
+
