dd724c97b7a242bd8a72eff8f07c8355ee363fd6
Sravanthi Kanchi <shravik593@gmail.com>
2017-12-13 23:49:24 -0500

    Wiggle Sort

---
+++ WiggleSortArray.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+
+
+// Time complexity - O(n) 
+
+
+class WiggleSortArray{
+
+    public void wiggleSort(int[] nums){
+      
+       for(int i = 0;i<nums.length - 1;i++){
+          if(i %2 == 0){
+             if(nums[i] > nums[i+1]){
+               swap(nums,i,i+1);
+             }
+           }
+           else if(nums[i] < nums[i+1]){
+              swap(nums,i,i+1);
+           }
+       } // end of for
+    
+    
+    } // end of function 
+    
+    
+    public void swap(int[] nums, int i , int j){
+    
+         int temp = nums[i];
+         nums[i] = nums[j];
+         nums[j] = temp; 
+    
+    
+    } 
+
+
+
+}
