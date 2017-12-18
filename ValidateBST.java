183a71fcde0a79ef9d5191a813ab4cfab89be3e0
Sravanthi Kanchi <shravik593@gmail.com>
2017-12-18 00:18:26 -0500

    check if Binary tree is BS

---
+++ ValidateBST.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+
+class ValidateBST{
+public boolean isValidBST(TreeNode root) {
+   
+       return isBST(root,Long.MIN_VALUE,Long.MAX_VALUE);
+    }
+    
+    public boolean isBST(TreeNode root, long min, long max){
+        if(root == null) return true;
+        if(root.val <= min || root.val >= max) return false; 
+        return isBST(root.left,min,root.val ) && isBST(root.right,root.val,max);
+    }
+
+
+}
